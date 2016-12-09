d13cc9990bb260683fb54e3d905ad3614aa3e97c
FujimoriHakuto <o109055h@mail.kyutech.jp>
2016-12-09 11:38:49 +0900

    text editor 01

--- editor.py
+++ editor.py
@@ -13,4 +13,25 @@ class UI(QMainWindow):
     
   def initUI(self):
     self.setGeometry(300,300,250,150)
-    self.setWindowTitle("Qt Text Text Sample")
+    self.setWindowTitle("Qt Text Editor")
+    
+    self.w = QWidget()
+    
+    self.text = QTextEdit(self)
+    hbox = QHBoxLayout()
+    hbox.addWidget(self.text)
+    
+    self.w.setLayout(hbox)
+    self.setCentralWidget(self.w)
+    
+    self.show()
+        
+    
+def main():
+  app = QApplication(sys.argv)
+  ui = UI()
+  sys.exit(app.exec_())
+  
+  
+if __name__ == "__main__":
+  main()
